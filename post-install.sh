32a01f55c1f2eb64768c0327de9034737e2b9e5a
imrisaac <imrisaac@gmail.com>
2025-02-28 13:23:20 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -12,11 +12,11 @@ rm -rf accel-sensor-bridge.deb
 
 
 # Install cuda and gpu drivers
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/sbsa/cuda-ubuntu2404.pin
-#mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
-#dpkg -i cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
-#cp /var/cuda-repo-ubuntu2404-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-#apt-get update
-#apt-get -y install cuda-toolkit-12-8
-#apt-get install -y nvidia-open
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/sbsa/cuda-ubuntu2404.pin
+mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
+dpkg -i cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
+cp /var/cuda-repo-ubuntu2404-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install cuda-toolkit-12-8
+apt-get install -y nvidia-open
